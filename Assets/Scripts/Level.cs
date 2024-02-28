6ee0f8a5ae5ee55968b36c9fa1eaa1ba7ee3bf71
Prismly <prismly55@gmail.com>
2024-02-27 20:18:29 -0500

    Scaffolding Obstacle System
    - Creating basic class skeletons for Level, Bot, Obstacle

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,33 +6,50 @@ using UnityEngine;
 // THIS IS MORESO A PROOF OF CONCEPT FOR A SYSTEM
 public class Level : MonoBehaviour
 {
+    public enum ObstacleType
+    {
+        WALK,
+        CLIMB,
+        JUMP,
+        LIFT,
+        SWIM
+    }
+
+    // Node class for specifying any state we want the path to have
+    public class Node
+    {
+        Vector2 position;
+        ObstacleType obsType;
+
+        public Node(Vector2 position, ObstacleType obsType)
+        {
+            this.position = position;
+            this.obsType = obsType;
+        }
+    }
+
     // The list of points that are the path the player will follow
-    List<Node> path;
+    private List<Node> path;
 
-    // References to the legs/arms themselves
-    [SerializeField] GameObject left;
-    [SerializeField] GameObject right;
-
-    // the indexes of each foot/arm in the node list
-    int leftIdx;
-    int rightIdx;
+    // By convention, [0] will be the player and [1] will be the computer
+    [SerializeField] private Vector2[] botOffs;
+    [SerializeField] private Bot[] bots;
+    [SerializeField] private GameObject botPref;
 
     // Start is called before the first frame update
     void Start()
     {
-        leftIdx = 0;
-        rightIdx = 1;
+        foreach (Transform c in transform)
+        {
+            path.Add(new Node(c.position, ObstacleType.WALK));
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    // Node class for specifying any state we want the path to have
-    public class Node
-    {
-        Vector3 position;
     }
 }
---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private Vector2 endpoint = Vector2.zero;
+    //[SerializeField] private 
+}
