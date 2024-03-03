709bba2615d7588b86a511997186191fdeb5bc94
FirebombDragon <nikolaus1204@gmail.com>
2024-03-03 13:00:15 -0500

    Small change in attempt to reduce desync

--- Assets/Scripts/Minimap/BigMapController.cs
+++ Assets/Scripts/Minimap/BigMapController.cs
@@ -15,42 +15,79 @@ public class BigMapController : MonoBehaviour
     private Material player1mat;
     [SerializeField]
     private Material player2mat;
-
+    private bool player1occupied;
+    private bool player2occupied;
+    private float colorchange;
 
     // Start is called before the first frame update
     void Start()
     {
         mapmaterial = GetComponent<Renderer>();
+        player1occupied = false;
+        player2occupied = false;
+        colorchange = 0f;
     }
 
-    // // Update is called once per frame
-    // void FixedUpdate()
-    // {
-        
-    // }
-
-    IEnumerator OnTriggerStay(Collider other)
+    // Update is called once per frame
+    void FixedUpdate()
     {
+        colorchange += Time.deltaTime;
+        if (colorchange >= 1f) {
+            colorchange = 0f;
+            if (player1occupied && !player2occupied) {
+                if (mapmaterial.material.color != basematerial.color) {
+                    mapmaterial.material = basematerial;
+                }
+                else {
+                    mapmaterial.material = player1mat;
+                }
+            }
+            else if (!player1occupied && player2occupied) {
+                if (mapmaterial.material.color != basematerial.color) {
+                    mapmaterial.material = basematerial;
+                }
+                else {
+                    mapmaterial.material = player2mat;
+                }
+            }
+            else if (player1occupied && player2occupied) {
+                if (mapmaterial.material.color != player1mat.color) {
+                    mapmaterial.material = player1mat;
+                }
+                else {
+                    mapmaterial.material = player2mat;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        mapmaterial.material = basematerial;
+        colorchange = 0f;
             GameObject otherObj = other.gameObject;
             if (otherObj == player1 || otherObj == player2) {
                 if (otherObj == player1) {
-                    if (mapmaterial.material.color != basematerial.color) {
-                        mapmaterial.material = basematerial;
-                    }
-                    else {
-                        mapmaterial.material = player1mat;
-                    }
+                    player1occupied = true;
                 }
                 else if (otherObj == player2) {
-                    if (mapmaterial.material.color != basematerial.color) {
-                        mapmaterial.material = basematerial;
-                    }
-                    else {
-                        mapmaterial.material = player2mat;
-                    }
+                    player2occupied = true;
                 }
             }
-        
-        yield return new WaitForSeconds(5);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        mapmaterial.material = basematerial;
+        colorchange = 0f;
+        GameObject otherObj = other.gameObject;
+            if (otherObj == player1 || otherObj == player2) {
+                if (otherObj == player1) {
+                    player1occupied = false;
+                }
+                else if (otherObj == player2) {
+                    player2occupied = false;
+                }
+            }
     }
 }
